ca48eb6ce94aa5db862533475068d16c59a27756
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2023-01-06 09:20:54 -0300

    (feat): collapse work experience

---
+++ src/components/common/overlay-trigger.tsx
@@ -0,0 +1,33 @@
+import Button from 'react-bootstrap/Button';
+import { Placement } from 'react-bootstrap/esm/types';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip, { TooltipProps } from 'react-bootstrap/Tooltip';
+
+interface ITriggerExampleProps {
+    element: JSX.Element
+    text: string
+    placement?: Placement    
+}
+
+function TriggerExample(props: ITriggerExampleProps) {
+    const { element, placement, text } = props;
+
+    const renderTooltip = (props: TooltipProps) => (
+        <Tooltip id="element-tooltip" {...props}>
+            { text }
+        {/* Simple tooltip */}
+        </Tooltip>
+    );
+
+    return (
+        <OverlayTrigger
+        placement={placement}
+        delay={{ show: 250, hide: 400 }}      
+        overlay={renderTooltip}
+        >
+            { element }
+        </OverlayTrigger>
+    );
+}
+
+export default TriggerExample;
--- src/components/work/work-experience.tsx
+++ src/components/work/work-experience.tsx
@@ -1,37 +1,65 @@
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Collapse, Row, Tooltip } from 'react-bootstrap';
 import IWorkExperienceModel from "../../models/work-experience-model";
 import styles from '../../../styles/Home.module.css'
+import React, { useState } from 'react';
+import TriggerExample from '../common/overlay-trigger';
 
 export const WorkExperience = (props: IWorkExperienceModel) => {
     const { role, company, from, to, city, country, description } = props;
+    const [open, setOpen] = useState(true);
+    
+    const handleOpen = () => {
+        setOpen(!open)
+    }
+
+    const el = (): JSX.Element => {
+        return (
+            <Button
+                variant="link"
+                onClick={handleOpen}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+            >
+                <i className={`bi bi-caret-${open ? 'down' : 'right'}-fill`}></i>
+            </Button>
+        )
+    }
 
     return (
         <Row>
             <Col xs={1}>
                 <div className={styles.right}>
-                    <i className="bi bi-caret-right-fill"></i>
+                    <TriggerExample element={el()} placement={'top'} text={open ? 'Hide details' : 'Show details'}/>
                 </div>
             </Col>
             <Col>
-                <div className="pt-2">
-                    <h3>role: {role}</h3>
+                <div className="pt-2">                    
+                    <h3><b>Role</b>: {role}</h3>
                 </div>
-                <div>
-                    <h4>company: {company}</h4>
-                </div>
-                <div>
-                    <h5>from: {from} to: {to}</h5>
-                </div>
-                <div>
-                    <i className="bi bi-geo-alt-fill"></i> City: {city}, Country: {country}
-                </div>
-                
-                <hr className="mt-3" />
-    
-                <div className="pb-2">
-                    <span>Description</span>
-                    <p>{description}</p>
-                </div>
+
+                <Collapse in={open}>
+                    <div id="example-collapse-text" className="pb-2">
+                        
+                        <div>
+                            <h4>Company: {company}</h4>
+                        </div>
+                        <div>
+                            <h5>From: {from} To: {to}</h5>
+                        </div>
+                        <div>
+                            <i className="bi bi-geo-alt-fill"></i> City: {city}, Country: {country}
+                        </div>
+                        
+                        <hr className="mt-3" />
+
+                        <div>
+                            <span>Description</span>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </Collapse>
+
+                <hr className={open ? 'd-none' : 'd-block'} />
             </Col>
         </Row>
     );
