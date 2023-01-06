42105cfc4072d3302464737f89209824b1873c9b
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2023-01-05 22:20:33 -0300

    (fix): email link

--- src/components/personal-section.tsx
+++ src/components/personal-section.tsx
@@ -21,19 +21,27 @@ const PersonalSection = (): JSX.Element => {
                     />
             </div>
             <div className={styles.center}>
-                <h1>RAMON FELIPE ALVES DE ARRUDA</h1>
+                <h1 className="d-none d-md-block d-lg-none">RAMON FELIPE ALVES DE ARRUDA</h1>
+                <h1 className="d-none d-sm-block d-md-none">RAMON FELIPE DE ARRUDA</h1>
+                <h1 className="d-block d-sm-none">RAMON FELIPE</h1>
             </div>
-            <Row>
-                <Col className={styles.center}>              
+            <Row className={styles.center}>
+                <Col className={styles.center} sm={12} md={4} xl={3}>              
                     <span><b>GENDER: </b>Male</span>
                 </Col>
-                <Col className={styles.center}>
+                <Col className={styles.center} sm={12} md={4} xl={3}>
                     <span><b>NATIONALITY: </b>Brazilian</span>
                 </Col>
             </Row>
-            <Row>
-                <Col className={styles.center}>
-                    <i className="bi bi-envelope-at-fill pe-2 " style={{fontSize: '2rem', color: 'cornflowerblue'}}></i><span><b>EMAIL: </b>ramon.felipe@hotmail.com</span>
+            <Row className={styles.center}>
+                <Col xs={1} className="w-auto p-0">
+                    <i className="bi bi-envelope-at-fill" style={{fontSize: '2rem', color: 'cornflowerblue'}}></i>
+                </Col>
+                <Col xs={3}>
+                    <a href="mailto:ramon.felipe@hotmail.com?subject=Contato">
+                        <b>EMAIL: </b>
+                        <span>ramon.felipe@hotmail.com</span>
+                    </a>
                 </Col>
             </Row>
             <div>
