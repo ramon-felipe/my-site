14eda3480ba63985fb7ad267c930cfb52b89ba88
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2023-01-18 18:21:33 -0300

    feat: tooltip digital skills

--- src/components/digital-skills/digital-skills-section.tsx
+++ src/components/digital-skills/digital-skills-section.tsx
@@ -1,10 +1,12 @@
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, OverlayTrigger } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next';
 import styles from '../../../styles/Home.module.css'
 import digitalSkillsData from '../../data/digital-skills-data'
 import IDigitalSkillModel from '../../models/digital-skills-model';
-import { writeRate } from '../../utils/rate-utils';
+import { getRateText, writeRateWithIcons } from '../../utils/rate-utils';
 import '../../extensions/string-extensions'
+import React from 'react';
+import TriggerTooltip from '../common/overlay-trigger';
 
 const DigitalSkillsSection = (): JSX.Element => {
     const { t } = useTranslation();
@@ -23,7 +25,10 @@ const DigitalSkillsSection = (): JSX.Element => {
                         return (
                             <Col sm={4} md={3} key={i} className='p-2 m-2 border border-2'>
                                 <div className={styles.center}>{t(d.skillName).UpperCaseFirstLetter()}</div>
-                                <div className={styles.center}>{writeRate(d.rate)}</div>
+                                
+                                <TriggerTooltip text={t(getRateText(d.rate)).UpperCaseFirstLetter()}>
+                                    <div className={styles.center}>{writeRateWithIcons(d.rate)}</div>
+                                </TriggerTooltip>
                             </Col>
                         )
                     })
--- src/components/language/language-skill-rate.tsx
+++ src/components/language/language-skill-rate.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import styles from '../../../styles/Home.module.css';
-import { writeRate } from "../../utils/rate-utils";
+import { getRateText, writeRateWithIcons } from "../../utils/rate-utils";
+import TriggerTooltip from "../common/overlay-trigger";
 import { LanguageSkillRateProps } from "./language-skills";
+import { useTranslation } from 'react-i18next';
+import '../../extensions/string-extensions'
 
 export const LanguageSkillRate = (props: LanguageSkillRateProps) => {
+    const { t } = useTranslation();
     const { skillName, rate } = props;
 
     return (
@@ -12,7 +16,10 @@ export const LanguageSkillRate = (props: LanguageSkillRateProps) => {
             <Row className={styles.center}>
                 <Col className='p-2 m-2'>
                     <div className={styles.center}>{skillName}</div>
-                    <div className={styles.center}>{writeRate(rate)}</div>
+
+                    <TriggerTooltip text={t(getRateText(rate)).UpperCaseFirstLetter()}>
+                        <div className={styles.center}>{writeRateWithIcons(rate)}</div>
+                    </TriggerTooltip>
                 </Col>
             </Row>
         </Col>
